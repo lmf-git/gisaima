92e2d487b27333e17a7ea524d8f94c84bae0d985
lmf-git <contact@dreamsbyte.com>
2025-03-20 15:41:23 -0300

    Test game store and world select

--- src/lib/stores/game.js
+++ src/lib/stores/game.js
@@ -2,7 +2,7 @@ import { writable, derived, get as getStore } from 'svelte/store';
 import { browser } from '$app/environment';
 import { ref, onValue, get as dbGet, set } from "firebase/database";
 import { db } from '../firebase/database.js';
-import { user } from '../firebase/user.js'; // Assuming you have an auth store
+import { user } from './user.js'; // Changed from '../firebase/user.js'
 
 // Cache for world info to reduce database calls
 const worldInfoCache = new Map();
