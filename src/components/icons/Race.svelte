79fa13f792b1cc9ce58ed29ba5c91934b6fdffe8
lmf-git <contact@dreamsbyte.com>
2025-04-24 11:44:09 -0300

    wip race icon to shorten tmeplates

---
+++ src/components/icons/Race.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Human from './Human.svelte'
+    import Elf from './Elf.svelte'
+    import Dwarf from './Dwarf.svelte'
+    import Goblin from './Goblin.svelte'
+    import Fairy from './Fairy.svelte'
+
+    const { raceKey } = $props();
+    
+    const races = {
+        human: Human,
+        elf: Elf,
+        dwarf: Dwarf,
+        goblin: Goblin,
+        fairy: Fairy
+    }
+
+    const Icon = races[raceKey.toLowerCase()]
+</script>
+
+{ Icon }
