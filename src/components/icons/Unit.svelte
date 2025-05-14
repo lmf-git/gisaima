4ee7482725ab27f94e207ac87dedc00f28c48a34
lmf-git <contact@dreamsbyte.com>
2025-05-14 02:41:41 -0300

    add real unit/monster icon keys

--- src/components/icons/Unit.svelte
+++ src/components/icons/Unit.svelte
@@ -1,42 +1,66 @@
 <script>
-    import Player from './Player.svelte';
-    import Sword from './Sword.svelte';
-    import Boots from './Boots.svelte';
-    import Axe from './Axe.svelte';
-    import Dagger from './Dagger.svelte';
-    import Blades from './Blades.svelte';
-    import Eye from './Eye.svelte';
-    import Thorn from './Thorn.svelte';
-    import Wing from './Wing.svelte';
-    import Hammer from './Hammer.svelte';
-    import Pick from './Pick.svelte';
-    import Cross from './Cross.svelte';
-    import Staff from './Staff.svelte';
-    import Gear from './Gear.svelte';
-    import Shield from './Shield.svelte';
-    import Bow from './Bow.svelte';
-    import Pickaxe from './Pickaxe.svelte';
+    import HumanWarrior from './HumanWarrior.svelte';
+    import HumanScout from './HumanScout.svelte';
+    import GoblinWarrior from './GoblinWarrior.svelte';
+    import GoblinScout from './GoblinScout.svelte';
+    import ElfWarrior from './ElfWarrior.svelte';
+    import ElfScout from './ElfScout.svelte';
+    import FairyWarrior from './FairyWarrior.svelte';
+    import FairyScout from './FairyScout.svelte';
+    import DwarfWarrior from './DwarfWarrior.svelte';
+    import DwarfScout from './DwarfScout.svelte';
+    import Healer from './Healer.svelte';
+    import Shaman from './Shaman.svelte';
+    import SiegeEngineer from './SiegeEngineer.svelte';
+    import HumanKnight from './HumanKnight.svelte';
+    import ElfArcher from './ElfArcher.svelte';
+    import DwarfDefender from './DwarfDefender.svelte';
+    import GoblinRaider from './GoblinRaider.svelte';
+    import FairyEnchanter from './FairyEnchanter.svelte';
+    import EliteGuard from './EliteGuard.svelte';
+    import SiegeRam from './SiegeRam.svelte';
+    import MasterCraftsman from './MasterCraftsman.svelte';
+    import ResourceGatherer from './ResourceGatherer.svelte';
+    import Goblin from './Goblin.svelte';
+    import Wolf from './Wolf.svelte';
+    import Bandit from './Bandit.svelte';
+    import Spider from './Spider.svelte';
+    import Skeleton from './Skeleton.svelte';
+    import Troll from './Troll.svelte';
+    import Elemental from './Elemental.svelte';
 
     const { unitIconKey, extraClass = '' } = $props();
     
     const icons = {
-        player: Player,
-        sword: Sword,
-        boots: Boots,
-        axe: Axe,
-        dagger: Dagger,
-        blades: Blades,
-        eye: Eye,
-        thorn: Thorn,
-        wing: Wing,
-        hammer: Hammer,
-        pick: Pick,
-        cross: Cross,
-        staff: Staff,
-        gear: Gear,
-        shield: Shield,
-        bow: Bow,
-        pickaxe: Pickaxe
+        human_warrior: HumanWarrior,
+        human_scout: HumanScout,
+        goblin_warrior: GoblinWarrior,
+        goblin_scout: GoblinScout,
+        elf_warrior: ElfWarrior,
+        elf_scout: ElfScout,
+        fairy_warrior: FairyWarrior,
+        fairy_scout: FairyScout,
+        dwarf_warrior: DwarfWarrior,
+        dwarf_scout: DwarfScout,
+        healer: Healer,
+        shaman: Shaman,
+        siege_engineer: SiegeEngineer,
+        human_knight: HumanKnight,
+        elf_archer: ElfArcher,
+        dwarf_defender: DwarfDefender,
+        goblin_raider: GoblinRaider,
+        fairy_enchanter: FairyEnchanter,
+        elite_guard: EliteGuard,
+        siege_ram: SiegeRam,
+        master_craftsman: MasterCraftsman,
+        resource_gatherer: ResourceGatherer,
+        goblin: Goblin,
+        wolf: Wolf,
+        bandit: Bandit,
+        spider: Spider,
+        skeleton: Skeleton,
+        troll: Troll,
+        elemental: Elemental
     }
 
     const Icon = icons[unitIconKey?.toLowerCase()]
