f538da8168ede023d7ba61bbfac1c67cd08c0c5a
lmf-git <contact@dreamsbyte.com>
2025-05-14 03:07:36 -0300

    feat: update unit icons with detailed SVG paths for various units

--- src/components/icons/units/Healer.svelte
+++ src/components/icons/units/Healer.svelte
@@ -3,6 +3,5 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={color}>
-    <!-- Insert path data here -->
-    <path d="M12 2L2 7v10l10 5 10-5V7L12 2z"/>
+    <path d="M10.5 13H8v-3h2.5V7.5h3V10H16v3h-2.5v2.5h-3V13zM12 2L4 5v6c0 5.55 3.84 10.74 8 12 4.16-1.26 8-6.45 8-12V5l-8-3zm0 2c2.8 0 5 2.2 5 5s-2.2 5-5 5-5-2.2-5-5 2.2-5 5-5z"/>
 </svg>
--- src/components/icons/units/SiegeEngineer.svelte
+++ src/components/icons/units/SiegeEngineer.svelte
@@ -3,6 +3,5 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={color}>
-    <!-- Insert path data here -->
-    <path d="M12 2L2 7v10l10 5 10-5V7L12 2z"/>
+    <path d="M12 3L2 12h3v8h14v-8h3L12 3zm0 2.2L17.8 10H16v8h-2v-4h-4v4H8v-8H6.2L12 5.2zm-1 7.8h2v2h-2v-2z"/>
 </svg>
