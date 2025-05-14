4d6830123e1604e200fd207ad9226fac56a14416
lmf-git <contact@dreamsbyte.com>
2025-05-14 03:12:57 -0300

    Refactor unit icons: Update SVG paths for DwarfDefender, DwarfScout, DwarfWarrior, Elemental, ElfArcher, ElfScout, ElfWarrior, EliteGuard, FairyEnchanter, FairyScout, FairyWarrior, GoblinRaider, GoblinScout, GoblinWarrior, and add Ork icon.

--- src/components/icons/units/ElfWarrior.svelte
+++ src/components/icons/units/ElfWarrior.svelte
@@ -3,6 +3,5 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={size} height={size} fill={color}>
-    <!-- Insert path data here -->
-    <path d="M12 2L2 7v10l10 5 10-5V7L12 2z"/>
+    <path d="M12 2L9 5h6L12 2zM6 7c-1.1 0-2 0.9-2 2s0.9 2 2 2 2-0.9 2-2S7.1 7 6 7zM18 7c-1.1 0-2 0.9-2 2s0.9 2 2 2 2-0.9 2-2S19.1 7 18 7zM8 12l-3 7h2l2-4v7h2v-7h2v7h2v-7l2 4h2l-3-7H8zM4 13v5l2 2v-5L4 13zM20 13l-2 2v5l2-2V13z"/>
 </svg>
