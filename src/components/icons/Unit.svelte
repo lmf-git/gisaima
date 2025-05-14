df40389d866529c69ff11a89953571a2350b4269
lmf-git <contact@dreamsbyte.com>
2025-05-14 02:37:28 -0300

    Creating unit icon file wrong entries for now.

--- src/components/icons/Unit.svelte
+++ src/components/icons/Unit.svelte
@@ -1,24 +1,26 @@
 <script>
-    import Sword from './icons/Sword.svelte';
-    import Boots from './icons/Boots.svelte';
-    import Axe from './icons/Axe.svelte';
-    import Dagger from './icons/Dagger.svelte';
-    import Blades from './icons/Blades.svelte';
-    import Eye from './icons/Eye.svelte';
-    import Thorn from './icons/Thorn.svelte';
-    import Wing from './icons/Wing.svelte';
-    import Hammer from './icons/Hammer.svelte';
-    import Pick from './icons/Pick.svelte';
-    import Cross from './icons/Cross.svelte';
-    import Staff from './icons/Staff.svelte';
-    import Gear from './icons/Gear.svelte';
-    import Shield from './icons/Shield.svelte';
-    import Bow from './icons/Bow.svelte';
-    import Pickaxe from './icons/Pickaxe.svelte';
+    import Player from './Player.svelte';
+    import Sword from './Sword.svelte';
+    import Boots from './Boots.svelte';
+    import Axe from './Axe.svelte';
+    import Dagger from './Dagger.svelte';
+    import Blades from './Blades.svelte';
+    import Eye from './Eye.svelte';
+    import Thorn from './Thorn.svelte';
+    import Wing from './Wing.svelte';
+    import Hammer from './Hammer.svelte';
+    import Pick from './Pick.svelte';
+    import Cross from './Cross.svelte';
+    import Staff from './Staff.svelte';
+    import Gear from './Gear.svelte';
+    import Shield from './Shield.svelte';
+    import Bow from './Bow.svelte';
+    import Pickaxe from './Pickaxe.svelte';
 
     const { unitIconKey, extraClass = '' } = $props();
     
     const icons = {
+        player: Player,
         sword: Sword,
         boots: Boots,
         axe: Axe,
